b5aae50333830b8a912b6cd876fb928fa4b314af
Nick <90702473+codesshaman@users.noreply.github.com>
2022-11-23 08:32:25 -0500

    Create first_master_node_setup.sh

---
+++ first_master_node_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# add k3s in env
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1  --bind-address=$1 --advertise-address=$1 "
+
+# download and run k3s agent
+curl -sfL https://get.k3s.io | K3S_TOKEN='^SH{Ge-jhWHdP<X"lX0A' sh -s - server --cluster init
+
+# copy master node token for worker node
+sudo cp /var/lib/rancher/k3s/server/node-token /mnt/scripts/
